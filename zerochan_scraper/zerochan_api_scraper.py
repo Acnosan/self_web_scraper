8381a80c783533297b573dbaa0f7498009d28e0d
Acnosan <abdelhak.meze4@.com>
2024-12-31 12:49:27 +0100

    fixed 503 issues

--- zerochan_scraper/zerochan_api_scraper.py
+++ zerochan_scraper/zerochan_api_scraper.py
@@ -1,11 +1,17 @@
 import os
 import time
+import random
 import requests
 import concurrent.futures as THREAD
 from datetime import datetime
 from bs4 import BeautifulSoup
-MAX_WORKERS_EXTRACT_SRCS = 15
-MAX_WORKERS_DOWNLOAD_IMAGES = 20
+from tqdm import tqdm
+from threading import Lock
+from urllib3.util.retry import Retry
+
+MAX_WORKERS_EXTRACT_SRCS = 5
+MAX_WORKERS_DOWNLOAD_IMAGES = 8
+# CAUSE HERE WE WILL HAVE TWO PROCESSES THAT USES THREADS TOTAL IS 20 SO MAX POOL SIZE > 20
 
 class ZeroChanScraper():
 
@@ -18,10 +24,30 @@ class ZeroChanScraper():
         self.file_name = file_name
         self.stopped_at_download_idx = stopped_at_download_idx
         self.session = None
+        self.progress_bar = None
+        self.lock = Lock()
 
+    def _make_session(self):
+        session = requests.Session()
+        adapter = requests.adapters.HTTPAdapter(
+            pool_connections=30,
+            pool_maxsize=30,
+            max_retries=Retry(
+                total=4,
+                backoff_factor=1,  # Wait: 1s, 2s, 4s, etc.
+                status_forcelist=[503]
+            )
+        )
+        session.headers.update({
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+            "Referer": "https://www.zerochan.net"
+        })
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+        return session
+    
     def extract_posts(self):
         search_url = f"{self.base_url}/{self.tag}" + (f"?p={self.page_idx}" if self.page_idx>1 else '')
-
         all_ids = set()
         retrieved_posts_ids = []
         try:
@@ -29,10 +55,8 @@ class ZeroChanScraper():
             if response.status_code != 200:
                 print(f"{response.status_code} is the status code : not 200")
                 return []
-            print(f"\nLoading on page {self.page_idx}\n")
-            
+            #print(f"\nLoading on page {self.page_idx}\n")
             soup = BeautifulSoup(response.text, 'html.parser')
-            
             posts_section = soup.find('ul', class_='medium-thumbs')
             if not posts_section:
                 print("No posts container found")
@@ -42,12 +66,11 @@ class ZeroChanScraper():
                 extracted_id = post.get('data-id')
                 if extracted_id:
                     all_ids.add(extracted_id)
-                if self.max_images_posts >= len(all_ids):
-                    self.page_idx +=1
-                    
+            if self.max_images_posts >= len(all_ids):
+                self.page_idx +=1
             retrieved_posts_ids = list(all_ids)[:max_images_posts]
             
-            print(f"Found {len(all_ids)} posts on page, retrieved {len(retrieved_posts_ids)}, (limited by max_images_posts which left {self.max_images_posts} )")
+            #print(f"Found {len(all_ids)} posts on page, retrieved {len(retrieved_posts_ids)}, (limited by max_images_posts which left {self.max_images_posts} )")
             return retrieved_posts_ids
         except Exception as e:
             print(f"Error occurred in extract_posts: {e}")
@@ -72,20 +95,6 @@ class ZeroChanScraper():
         # Return a list of tuples with image URLs and the post_id
         return images_srcs
 
-    def _make_session(self):
-        session = requests.Session()
-        adapter = requests.adapters.HTTPAdapter(
-            pool_connections=20,
-            pool_maxsize=20,
-            max_retries=3
-        )
-        session.headers.update({
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
-        })
-        session.mount('http://', adapter)
-        session.mount('https://', adapter)
-        return session
-    
     def download_image(self,session,idx,image_src,post_id):
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         image_name = f"{self.file_name}_idx_{idx + 1:04d}_{timestamp}.jpg"
@@ -103,13 +112,16 @@ class ZeroChanScraper():
             try:
                 with open(image_path, 'wb') as image:
                     image.write(response.content)
+                with self.lock:
+                    self.progress_bar.update(1)
+                    time.sleep(0.0005)
             except Exception as e:
                 print(f"Image {image_src} Is Not Downloaded")
         except Exception as e:
             print(f"Error downloading image {image_name}:{e}")
 
     def multi_threading_extract_srcs(self, posts_ids):
-        print("Multi Threading Of SRCs Extraction BEGIN....")
+        #print("Multi Threading Of SRCs Extraction BEGIN....")
         combined_images_srcs = []
         combined_images_ids = []
         with THREAD.ThreadPoolExecutor(max_workers=MAX_WORKERS_EXTRACT_SRCS) as EXE:
@@ -127,12 +139,11 @@ class ZeroChanScraper():
                 print(f"Exception Occurred During Multi Threading Extract Srcs: {e}")
                 return []
         
-        print(f"All Srcs Located With Total {len(combined_images_srcs)}")
+        #print(f"All Srcs Located With Total {len(combined_images_srcs)}")
         return combined_images_srcs,combined_images_ids
 
     def multi_threading_download_images(self,images_srcs,posts_ids):
-        print("Multi Threading Of Downloading Images BEGIN....")
-        self.session = self._make_session()
+        #print("Multi Threading Of Downloading Images BEGIN....")
         with THREAD.ThreadPoolExecutor(max_workers=MAX_WORKERS_DOWNLOAD_IMAGES) as executor:
             executor.map(
                 lambda args: self.download_image(self.session, *args),
@@ -146,10 +157,18 @@ class ZeroChanScraper():
     def scrape(self):
         os.makedirs(self.output_folder,exist_ok=True)
         try:
-            self.session = self._make_session()
+            self.progress_bar = tqdm(
+                total=self.max_images_posts,
+                desc="Scraping Images",
+                unit="Image",
+                bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}]",
+                colour='red',
+                leave=True
+            )
             while self.max_images_posts > 0:
                 if self.max_images_posts <= 0:
                     break
+                self.session = self._make_session()
                 retrieved_posts_ids = self.extract_posts()
                 if len(retrieved_posts_ids) == 0:
                     print("extract_posts returned nothing...")
@@ -167,8 +186,6 @@ class ZeroChanScraper():
                     continue
         except Exception as e:
             print(f"Error Occured in Scrape Method : {e}")
-        finally:
-            print(f"Finished.. Quiting Process START")
 
 def count_images_os(folder_path):
     image_extensions = ('.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp')
@@ -178,7 +195,7 @@ def count_images_os(folder_path):
 if __name__ == "__main__":
     tag = "Hoshimi Miyabi"
     page_idx = 1
-    max_images_posts = 10
+    max_images_posts = 200
     stopped_at_download_idx = 0
     
     os.makedirs('../scraped_datasets', exist_ok=True)
@@ -198,4 +215,4 @@ if __name__ == "__main__":
     
     scraper.scrape()
     end_time = time.time()
-    print(f"Scaper took {(end_time-begin_time):.2f} seconds with final result of {count_images_os(output_folder)} images.")
+    print(f" Finished.. Scaper took {(end_time-begin_time):.2f} seconds with final result of {count_images_os(output_folder)} images.")
